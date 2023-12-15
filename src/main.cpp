72fbe928b4b71b2de3385914acddcf25b328dfe9
Ryan Johanningmeier <me@ryanjo.com>
2023-12-15 09:42:57 -0600

    Hello complete

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,12 @@
 // Using SDL and standard IO
+#include <SDL3/SDL_error.h>
+#include <SDL3/SDL_events.h>
+#include <SDL3/SDL_init.h>
+#include <SDL3/SDL_oldnames.h>
+#include <SDL3/SDL_pixels.h>
+#include <SDL3/SDL_surface.h>
+#include <SDL3/SDL_video.h>
+#include <cstddef>
 #include <stdio.h>
 #include <SDL3/SDL.h>
 
@@ -19,4 +27,53 @@ int main(int argc, char* args[])
   {
     printf("SDL could not be initialized! SDL_Error: %s\n", SDL_GetError());
   }
+  else
+  {
+    // Create window
+    // SDL2 Implementation
+    // window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOWEVENT_SHOWN);
+    // SDL3 Implementation
+    window = SDL_CreateWindow("SDL Tutorial", SCREEN_HEIGHT, SCREEN_WIDTH, SDL_WINDOW_RESIZABLE);
+    if (window == NULL)
+    {
+      printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+    }
+    else
+    {
+      // Get window surface
+      screenSurface = SDL_GetWindowSurface(window);
+
+      // Fill the surface white
+      // SDL2 Implementation
+      // SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+      SDL_FillSurfaceRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+
+      // Update the surface
+      SDL_UpdateWindowSurface(window);
+
+      // Hack to get widow to stay up 
+      // This hack is for MacOS
+      SDL_Event e;
+      bool quit = false;
+      while(quit == false)
+      {
+        while(SDL_PollEvent(&e))
+        {
+          // SDL2 Implementation
+          // if (e.type == SDL_QUIT)
+          // SDL3 Implementation
+          if (e.type == SDL_EVENT_QUIT)
+            quit = true;
+        }
+      }
+    }
+  }
+
+  // Destroy window
+  SDL_DestroyWindow(window);
+
+  // Quit SDL subsystems
+  SDL_Quit();
+
+  return 0;
 }
