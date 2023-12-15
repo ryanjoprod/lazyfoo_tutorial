d0132ca71ffad6bedd7b132b137d87c80dd2de14
Ryan Johanningmeier <me@ryanjo.com>
2023-12-15 07:31:19 -0600

    Branch change

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,22 @@
-#include <iostream>
+// Using SDL and standard IO
+#include <stdio.h>
+#include <SDL3/SDL.h>
 
-int main()
+// Screen dimension constants
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+int main(int argc, char* args[])
 {
-  std::cout << "Hello world!" << std::endl;
+  // The window we'll be rending to
+  SDL_Window* window = NULL;
 
-  return 0;
+  // The surface contained by the window
+  SDL_Surface* screenSurface = NULL;
+
+  // Initialize SDL
+  if (SDL_Init(SDL_INIT_VIDEO) < 0)
+  {
+    printf("SDL could not be initialized! SDL_Error: %s\n", SDL_GetError());
+  }
 }
