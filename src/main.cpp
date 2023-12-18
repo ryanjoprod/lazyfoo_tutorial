ebd41b8aac225422b78c1a9fbd27a4ee75143942
Ryan Johanningmeier <me@ryanjo.com>
2023-12-18 07:08:57 -0600

    Loads multiple image types

--- src/main.cpp
+++ src/main.cpp
@@ -118,6 +118,31 @@ bool loadMedia()
     success = false;
   }
 
+  gKeyPressSurfaces[KEY_PRESS_SURFACE_UP] = loadSurface("images/up.bmp");
+  if (gKeyPressSurfaces[KEY_PRESS_SURFACE_UP] == NULL)
+  {
+    printf("Failed to load up image!\n");
+    success = false;
+  }
+
+  gKeyPressSurfaces[KEY_PRESS_SURFACE_DOWN] = loadSurface("images/down.bmp");
+  if (gKeyPressSurfaces[KEY_PRESS_SURFACE_DOWN] == NULL)
+  {
+    printf("Failed to load down image!\n");
+    success = false;
+  }
+  gKeyPressSurfaces[KEY_PRESS_SURFACE_LEFT] = loadSurface("images/left.bmp");
+  if (gKeyPressSurfaces[KEY_PRESS_SURFACE_LEFT] == NULL)
+  {
+    printf("Failed to load left image!\n");
+    success = false;
+  }
+  gKeyPressSurfaces[KEY_PRESS_SURFACE_RIGHT] = loadSurface("images/right.bmp");
+  if (gKeyPressSurfaces[KEY_PRESS_SURFACE_RIGHT] == NULL)
+  {
+    printf("Failed to load right image!\n");
+    success = false;
+  }
   return success;
 }
 
